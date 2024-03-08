63d89e358a6d5d79ab1aba04b2b8adeae26ea34b
NinoosMoshi <ninos1357@yahoo.com>
2024-03-08 15:14:01 -0600

    add ngx bootstrap

---
+++ frontEnd/shop4u-ui/src/main.ts
@@ -0,0 +1,16 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes)
+  ]
+})
+  .catch((err) => console.error(err));
